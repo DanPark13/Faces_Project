5cd02c3586b76f7af0e02f6a64b3c5961be51791
Daniel Park <37126844+DanPark13@users.noreply.github.com>
2020-11-16 20:02:26 -0500

    Added in load database for nonmask images as well

--- load_database.m
+++ load_database.m
@@ -26,3 +26,61 @@ if(isempty(loadedImage))
 end
 loadedImage = ""; % Needs to return to empty next time image needs to be loaded
 output_image = numImage; 
+
+%%% NonMask Portion
+%% Description
+% Loads the no mask dataset in preparation for face recognition
+
+%% Variables
+loadedImage = [];
+numImage = [];
+
+%% Dataset Loading Function
+if(isempty(loadedImage)) 
+    allImages = zeros(112*92,... % Pixels of each image
+        10); % Number of folders representing a single person
+    cd(strcat('no_mask_set')); 
+    for i=1:10 
+        cd(strcat('s',num2str(i))); 
+        for j=1:10 
+            imageContainer = imread(strcat(num2str(j),'.pgm')); 
+            allImages(:,(i-1)*10+j)... % get all images from each folder
+                = reshape(imageContainer,size(imageContainer,1)... 
+                *size(imageContainer,2),1); % reshape images into arrays for analysis
+        end
+        cd .. % changes folder directory back to load other faces
+    end
+    cd ..
+    numImage = uint8(allImages); % changes all the images within the database to 8-bit images
+end
+loadedImage = ""; % Needs to return to empty next time image needs to be loaded
+outputImage = numImage; 
+
+%%% Mask Portion
+%% Description
+% Loads the mask dataset in preparation for face recognition
+
+%% Variables
+loadedImage1 = [];
+numImage1 = [];
+
+%% Dataset Loading Function
+if(isempty(loadedImage1)) 
+    allImages1 = zeros(112*92,... % Pixels of each image 
+        10); % Number of folders representing a single person
+    cd(strcat('mask_set')); 
+    for i=1:10 
+        cd(strcat('s',num2str(i))); 
+        for j=1:10 
+            imageContainer1 = imread(strcat(num2str(j),'.pgm')); 
+            allImages1(:,(i-1)*10+j)... % get all images from each folder
+                = reshape(imageContainer1,size(imageContainer1,1)... 
+                *size(imageContainer1,2),1); % reshape images into arrays for analysis
+        end
+        cd .. % changes folder directory back to load other faces
+    end
+    cd ..
+    numImage1 = uint8(allImages1); % changes all the images within the database to 8-bit images
+end
+loadedImage1 = ""; % Needs to return to empty next time image needs to be loaded
+outputImage1 = numImage1; 
